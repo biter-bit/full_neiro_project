d97e0af92844ecf83e8776239e963a6fb235fe1a
Миша Курашев <kurashevmichael@gmail.com>
2024-09-26 18:51:30 +0300

    update pay

--- web_app/main.py
+++ web_app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+import json
 
 app = FastAPI()
 
@@ -10,4 +11,9 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("success.html", {"request": request})
+    # Получаем все атрибуты request в виде словаря
+    request_data = vars(request)
+
+    # Сериализуем объект в JSON
+    request_data_json = json.dumps(request_data, default=str, indent=2)
+    return templates.TemplateResponse("success.html", {"request": request_data_json})
