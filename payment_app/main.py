22c9c936de04aad9d2bf1600ae5ef15f37775615
Миша Курашев <kurashevmichael@gmail.com>
2024-09-29 17:46:03 +0300

    new

--- payment_app/main.py
+++ payment_app/main.py
@@ -4,6 +4,9 @@ from fastapi.templating import Jinja2Templates
 from fastapi import Request
 import json
 import logging
+from db_api import api_invoice_async, api_profile_async
+from payment_app.db_api.models import Profile
+from services import robokassa_obj
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,6 +27,27 @@ def format_request_data(request):
         "Path Parameters": dict(request.path_params)
     }
     return json.dumps(request_data, indent=4, ensure_ascii=False)
+
+@app.get('/result', response_class=HTMLResponse)
+async def result_confirm(request: Request):
+    invoice = await api_invoice_async.get_invoice(request.get("InvId"))
+    if not invoice:
+        logger.error(f"Not invoice ERROR | {invoice}")
+        return "ERROR"
+
+    price = request.get("OutSum")
+    inv_id = request.get("InvId")
+    email = request.get("EMail")
+    signature = request.get("SignatureValue")
+
+    if not robokassa_obj.check_signature(inv_id=inv_id, price=price, recv_signature=signature):
+        logger.error(f"Check signature ERROR | {inv_id}")
+        return "Check signature ERROR"
+
+    if email and invoice.profile.email != email.lower():
+        await api_profile_async.update_email(invoice.profiles.id, email.lower())
+    profile = await api_profile_async.update_tariff_of_profile(invoice.profiles.id, 2)
+    return f"OK{inv_id}"
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
